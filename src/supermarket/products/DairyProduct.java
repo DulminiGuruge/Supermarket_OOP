7cdc0fd81250a5947736e9d066a43a1414a340cb
Dulmini Guruge <agg362@usask.ca>
2023-11-08 23:06:25 -0600

    Created inheritence in the Beverage class

--- src/supermarket/products/DairyProduct.java
+++ src/supermarket/products/DairyProduct.java
@@ -1,6 +1,30 @@
 package supermarket.products;
 
-public class DairyProduct {
+/**
+ * Implemented the concept of inheritence using the extend keyword to inherit from 
+ * the parent class
+ */
+
+public class DairyProduct extends GroceryProduct {
     private Fat Fat;
+
+    public DairyProduct(String name, double price, double discount, Fat Fat) {
+        super(name, price, discount);
+        this.Fat = Fat;
+    }
+
+    public Fat getFat(){
+        return Fat;
+    }
+
+    public void setFat(Fat Fat){
+        this.Fat = Fat;
+    }
+
+    
+
+    
+
+    
     
 }
